98c0860303eb527977635fae79acecd720ea3767
Jorge Celaya <jcelaya775@gmail.com>
2025-10-23 23:38:51 -0500

    chore: clean up

--- cmd/root.go
+++ cmd/root.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+Copyright © 2025 Jorge Celaya jcelaya775@gmail.com
 */
 package cmd
 
@@ -9,18 +9,12 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "gwt",
 	Short: "A git worktree wrapper that makes life easier\n\n",
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
 	/// TODO: Show TUI when no subcommand is provided
-	// Run: func(cmd *cobra.Command, args []string) { },
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
@@ -29,13 +23,6 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
+	// TODO: Add config path flag
 	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.gwt.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
